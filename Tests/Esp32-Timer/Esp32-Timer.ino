f635f73be99fd7dd159a8e0d53e54e453bfb1e60
ArnoNym2 <65808670+ArnoNym2@users.noreply.github.com>
2024-05-01 21:49:48 +0200

    Half working Timer example

--- Tests/Esp32-Timer/Esp32-Timer.ino
+++ Tests/Esp32-Timer/Esp32-Timer.ino
@@ -1,12 +1,69 @@
-#define LED_BUILTIN 2
-volatile ledStatus = LOW;
+#include "ESP32TimerInterrupt.h"
+
+#define LED 4
+#define Poti 13
+#define checkInterval 1000  // Time in ms after which Counters are printed
+
+
+#define TIMER0_INTERVAL_S 2  // Zeitintervall in Sekunden
+
+volatile uint32_t Timer0Count = 0;
+
+hw_timer_t* timer = NULL;
+//hw_timer_t* watchdogTimer = NULL;
+volatile SemaphoreHandle_t timerSemaphore;
+
+void IRAM_ATTR onTimer() {
+  xSemaphoreGiveFromISR(timerSemaphore, NULL);
+  Timer0Count++;
+  digitalWrite(LED, !digitalRead(LED));
+}
+
+void ARDUINO_ISR_ATTR resetModule() {
+  ets_printf("reboot\n");
+  esp_restart();
+}
+
+
+
+ESP32Timer ITimer0(0);
+
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED, OUTPUT);
+  pinMode(Poti, INPUT);
+  Serial.begin(115200);
 
+  Serial.println("Project EPS32-Timer");
+  Serial.print("Compiled: ");
+  Serial.print(__DATE__);
+  Serial.print("\t");
+  Serial.println(__TIME__);
+
+  timerSemaphore = xSemaphoreCreateBinary();
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 1000000, true);
+
+  //watchdogTimer = timerBegin(0, 80, true);
+  //timerAttachInterrupt(watchdogTimer, &resetModule, true);   //attach callback
+  //timerAlarmWrite(watchdogTimer, 3000 * 1000, false);  //set time in us
+
+
+
+  //timerAlarmEnable(watchdogTimer);  //enable interrupt
+  timerAlarmEnable(timer);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  if (xSemaphoreTake(timerSemaphore, 0) == pdTRUE) {
+    Serial.print("Interrupt Nr.\t");
+    Serial.println(Timer0Count);
+  }
+  uint64_t timerValue = map(analogRead(Poti), 0, 4095, 100000, 2000000);
+  timerAlarmWrite(timer, timerValue, true);
+  timerAlarmEnable(timer);
+  Serial.println(timerValue / 1000);
+
   delay(500);
 }
