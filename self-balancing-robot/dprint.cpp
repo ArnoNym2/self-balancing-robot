aa45a71964b66ce0b25f8ceed3aed4b60f4bd301
ArnoNym2 <65808670+ArnoNym2@users.noreply.github.com>
2024-05-17 22:23:45 +0200

    Add dprint() function as library

---
+++ self-balancing-robot/dprint.cpp
@@ -0,0 +1,81 @@
+#include "dprint.h"
+
+extern bool debugSetting;
+
+size_t nothing(){
+  return 0;
+}
+
+void dprintln(const char* message) {
+  debugSetting ? Serial.println(message) : nothing();
+}
+
+void dprintln(int message) {
+  debugSetting ? Serial.println(message) : nothing();
+}
+
+void dprintln(float message) {
+  debugSetting ? Serial.println(message, 2) : nothing(); // Print the float with 2 decimal places
+}
+
+void dprintln(double message) {
+  debugSetting ? Serial.println(message, 2) : nothing(); // Print the double with 2 decimal places
+}
+
+void dprintln(unsigned int message) {
+  debugSetting ? Serial.println(message) : nothing();
+}
+
+void dprintln(long message) {
+  debugSetting ? Serial.println(message) : nothing();
+}
+
+void dprintln(unsigned long message) {
+  debugSetting ? Serial.println(message) : nothing();
+}
+
+void dprintln(byte message) {
+  debugSetting ? Serial.println(message) : nothing();
+}
+
+void dprintln(bool message) {
+  debugSetting ? (Serial.println(message ? "True" : "False")) : nothing();
+}
+
+
+
+void dprint(const char* message) {
+  debugSetting ? Serial.print(message) : nothing();
+}
+
+void dprint(int message) {
+  debugSetting ? Serial.print(message) : nothing();
+}
+
+void dprint(float message) {
+  debugSetting ? Serial.print(message, 2) : nothing(); // Print the float with 2 decimal places
+}
+
+void dprint(double message) {
+  debugSetting ? Serial.print(message, 2) : nothing(); // Print the double with 2 decimal places
+}
+
+void dprint(unsigned int message) {
+  debugSetting ? Serial.print(message) : nothing();
+}
+
+void dprint(long message) {
+  debugSetting ? Serial.print(message) : nothing();
+}
+
+void dprint(unsigned long message) {
+  debugSetting ? Serial.print(message) : nothing();
+}
+
+void dprint(byte message) {
+  debugSetting ? Serial.print(message) : nothing();
+}
+
+void dprint(bool message) {
+  debugSetting ? (Serial.print(message ? "True" : "False")) : nothing();
+}
---
+++ self-balancing-robot/dprint.h
@@ -0,0 +1,46 @@
+#ifndef dprint_h
+#define dprint_h
+#include <Arduino.h>
+
+size_t nothing();
+
+void dprint(const char* message);
+
+void dprint(int message);
+
+void dprint(float message);
+
+void dprint(double message);
+
+void dprint(unsigned int message);
+
+void dprint(long message);
+
+void dprint(unsigned long message);
+
+void dprint(byte message);
+
+void dprint(bool message);
+
+
+void dprintln(const char* message);
+
+void dprintln(int message);
+
+void dprintln(float message);
+
+void dprintln(double message);
+
+void dprintln(unsigned int message);
+
+void dprintln(long message);
+
+void dprintln(unsigned long message);
+
+void dprintln(byte message);
+
+void dprintln(bool message);
+
+
+
+#endif
