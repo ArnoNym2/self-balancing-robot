96c388d66a6ffad590188f072a8c442d555f4c71
ArnoNym2 <65808670+ArnoNym2@users.noreply.github.com>
2024-05-24 10:50:33 +0200

    formatting

--- self-balancing-robot/settings.h
+++ self-balancing-robot/settings.h
@@ -9,16 +9,18 @@
 
 #define EEPROM_SIZE 256
 
-#define offsetAddress 0 //Address where the offsets are stored in EEPROM
+#define offsetAddress 0  //Address where the offsets are stored in EEPROM
 
-#define stepsPerRevolution 1600 // Steps needed for one rotation: With 1/8 microstepping its 200*8==1600
+#define pidAddress 30  //Address where the pid coefficients are stored in EEPROM
 
-#define maxNeededAngle 10 //maximum Angle where controler must work
+#define stepsPerRevolution 1600  // Steps needed for one rotation: With 1/8 microstepping its 200*8==1600
 
-#define ledBilinkTime 500 //How many ms should the led blink
+#define maxNeededAngle 10  //maximum Angle where controler must work
+
+#define ledBlinkTime 500  //How many ms should the led blink
 
 
-#define True true   //I'm dumb and probably will use both variants
+#define True true  //I'm dumb and probably will use both variants
 #define False false
 
 
