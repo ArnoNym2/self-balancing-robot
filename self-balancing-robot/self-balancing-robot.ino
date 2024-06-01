4f0317bb3274e9b4f5bf9094509f0ce9fd616986
ArnoNym2 <65808670+ArnoNym2@users.noreply.github.com>
2024-06-01 20:27:04 +0200

    Check the domain manually as abs() did not work (probably?)

--- self-balancing-robot/self-balancing-robot.ino
+++ self-balancing-robot/self-balancing-robot.ino
@@ -228,12 +228,12 @@ void setup() {
 void loop() {
   mpu.update();
   Input = mpu.getAngleY();
-  if (abs(Input > 90)) {  // The vehicle is laying
+   if (Input > 90 || Input < -90) {  // The vehicle is laying
     digitalWrite(stepperRight.enablePin, stepperRight.disable);
     digitalWrite(stepperLeft.enablePin, stepperLeft.disable);
     pid.SetMode(MANUAL);
     overMaximumAngle = true;
-  } else if (abs(Input < 90) && overMaximumAngle == true) {
+  } else if ((Input > -90 && Input < 90) && overMaximumAngle == true) {
     overMaximumAngle = false;
     digitalWrite(stepperRight.enablePin, !stepperRight.disable);
     digitalWrite(stepperLeft.enablePin, !stepperLeft.disable);
